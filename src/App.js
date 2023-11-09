1fec758bcecc65028dd56463f314bea20bad4654
Robert Stefan <office@rstefan.at>
2023-11-09 23:48:27 +0100

    removed warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './components/common/Header';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
