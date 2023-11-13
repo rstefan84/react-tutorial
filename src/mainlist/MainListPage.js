94e7e3481b472a31529e659115465557ce5b457b
Robert Stefan <office@rstefan.at>
2023-11-13 20:09:49 +0100

    01-07 ReactSimple - controlled Form

--- src/mainlist/MainListPage.js
+++ src/mainlist/MainListPage.js
@@ -41,6 +41,7 @@ function MainListPagePresentation({elements, changeFilter}) {
     <div>
       <h2>Main List Page</h2>
       <Filter changeFilter={changeFilter} />
+      <MainListEdit />
       <table className="table">
         <thead>
           <tr>
@@ -68,9 +69,87 @@ function Filter({changeFilter}) {
   
   return (
     <div>
-      <label htmlFor="filter">Filter :&nbsp;</label>
+      <label htmlFor="filter">Filter:&nbsp;</label>
       <input id='filter' value={filterValue} onChange={internalChangeFilter} />
     </div>
+  )
+}
+
+function MainListEdit() {
+  let defaultValues = {
+    position: '',
+    name: '',
+    weight: '',
+    symbol: ''
+  }
+  let [editValues,changeValues] = useState(defaultValues);
+
+  let onChangedSimpleInput = (inputId,value) => {
+    changeValues({
+      ...editValues,
+      [inputId]:value
+    })
+  }
+  
+  return(
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <SimpleInput inputId='position' 
+              size='3' 
+              name='Pos' 
+              defaultValue={editValues.position} 
+              onChanged={onChangedSimpleInput}
+            />
+          </td>
+          <td>
+            <SimpleInput inputId='name' 
+              size='15' 
+              name='Name' 
+              defaultValue={editValues.name} 
+              onChanged={onChangedSimpleInput}
+            />
+          </td>
+          <td>
+            <SimpleInput inputId='weight' 
+              size='6' 
+              name='Weight' 
+              defaultValue={editValues.weight} 
+              onChanged={onChangedSimpleInput}
+            />
+          </td>
+          <td>
+            <SimpleInput inputId='symbol' 
+              size='3' 
+              name='Symbol' 
+              defaultValue={editValues.symbol} 
+              onChanged={onChangedSimpleInput}
+            />
+          </td>
+          <td>
+            &nbsp;&nbsp;&nbsp;
+            <button>Save</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+function SimpleInput({inputId,size, name, defaultValue,onChanged}) {
+  let [value,setValue]=useState(defaultValue)
+  let onChangeInput = e => setValue(e.target.value)
+  let onBlurInput = e => onChanged(inputId,value)
+  return (
+    <>
+      <label htmlFor={inputId}>{name}:&nbsp;</label>
+      <input id={inputId} size={size}
+        value={value}
+        onChange={onChangeInput}
+        onBlur={onBlurInput}
+      />
+    </>
   )
 }
 
